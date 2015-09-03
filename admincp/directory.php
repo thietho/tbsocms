35772d0d3e3499368fce01e5936c688f23c9be29
thietho <thietho1982@gmail.com>
2015-09-03 14:48:03 +0700

    Cap nhat font

--- admincp/directory.php
+++ admincp/directory.php
@@ -14,12 +14,14 @@ define('DIR_CONTROLLER','controller/');
 define('DIR_MODEL','model/');
 define('DIR_LANGUAGE', 'language/');
 
-define('DIR_VIEW','view/skin1/');
-define('DIR_TEMPLATE','view/skin1/template/');
-define('DIR_IMAGE', 'view/skin1/image/');
-define('DIR_CSS', 'view/skin1/css/');
-define('DIR_JS', 'view/skin1/js/');
-define('DIR_XML', 'view/skin1/xml/');
+define('SKIN', 'skin2');
+define('DIR_VIEW','view/'.SKIN.'/');
+define('DIR_TEMPLATE','view/'.SKIN.'/template/');
+define('DIR_IMAGE', 'view/'.SKIN.'/image/');
+define('DIR_FLASH', 'view/'.SKIN.'/flash/');
+define('DIR_CSS', 'view/'.SKIN.'/css/');
+define('DIR_JS', 'view/'.SKIN.'/js/');
+define('DIR_XML', 'view/'.SKIN.'/xml/');
 
 //
 define('DIR_DATABASE', '../system/database/');
